6ed28ea887aa1f518e9d44273056cf332cf46765
Fionera <fionera@fionera.de>
2022-03-31 11:47:14 +0200

    README and Nix

---
+++ README.md
@@ -0,0 +1,22 @@
+# Spring Core RCE
+
+Build via
+```
+mvn package
+```
+
+Run via:
+
+```
+docker-compose up
+```
+
+Available on: 
+```
+http://127.0.0.1:8080
+```
+
+or via nix:
+```
+nix run
+```
---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in
+      rec {
+        packages = flake-utils.lib.flattenTree {
+          build-and-run =
+            pkgs.writeShellScriptBin "build-and-run" ''
+              ${pkgs.maven}/bin/mvn package
+              ${pkgs.docker-compose}/bin/docker-compose up
+            '';
+        };
+        apps.build-and-run = flake-utils.lib.mkApp { drv = packages.build-and-run; };
+        defaultPackage = packages.build-and-run;
+        defaultApp = apps.build-and-run;
+        devShell = pkgs.mkShell { buildInputs = [ packages.build-and-run ]; };
+      }
+    );
+}
